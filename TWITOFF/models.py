662fd8eef0065d0e838bdd2d531ce8a32187170b
hyamynl619 <amy.nguyenle619@gmail.com>
2020-02-25 12:09:56 -0500

    fixing minor bugs

--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -1,13 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 
-DB = SQLAlchemy
+
+DB = SQLAlchemy()
 
 class User (DB.Model):
     """ Twitter users that we analyze """
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
 
-    class Tweet (DB.Model):
-        id = DB.Column(DB.BigInteger, primary_key=True)
-        text = DB.Column(DB.Unicode(280))
+class Tweet (DB.Model):
+    """Tweets we pull"""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(280))
     
