81073087ecac3bdb689a56636f7dc332ae14620f
hyamynl619 <amy.nguyenle619@gmail.com>
2020-02-26 17:07:39 -0500

    new

--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -6,7 +6,7 @@ DB = SQLAlchemy()
 class User (DB.Model):
     """ Twitter users that we analyze """
     id = DB.Column(DB.BigInteger, primary_key=True)
-    name = DB.Column(DB.String(15), nullable=False)
+    name = DB.Column(DB.String(15))
     newest_tweet_id = DB.column(DB.BigInteger) 
     def __repr__(self):
         return '<User {}>'.format(self.name)
@@ -15,8 +15,7 @@ class Tweet (DB.Model):
     """Tweets we pull"""
     id = DB.Column(DB.BigInteger, primary_key=True)
     text = DB.Column(DB.Unicode(280))
-    user_id = DB.column(DB.BigInteger, DB.ForeignKey('user.id'), 
-    nullable = False)
+    user_id = DB.column(DB.BigInteger, DB.ForeignKey('user.id'))
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
     def __repr__(self):
         return 'Tweet {}>'.format(self.text)
