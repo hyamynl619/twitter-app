4d737d79d18c1e8a9b90af1fd835549bb3e8270d
hyamynl619 <amy.nguyenle619@gmail.com>
2020-02-26 20:57:14 -0500

    fixing bugs again

--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -3,20 +3,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 DB = SQLAlchemy()
 
+
 class User (DB.Model):
     """ Twitter users that we analyze """
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15))
-    newest_tweet_id = DB.column(DB.BigInteger) 
+    newest_tweet_id = DB.column(DB.BigInteger)
+
     def __repr__(self):
         return '<User {}>'.format(self.name)
+
 
 class Tweet (DB.Model):
     """Tweets we pull"""
     id = DB.Column(DB.BigInteger, primary_key=True)
-    text = DB.Column(DB.Unicode(280))
-    user_id = DB.column(DB.BigInteger, DB.ForeignKey('user.id'))
+    text = DB.Column(DB.Unicode(300))
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+
     def __repr__(self):
         return 'Tweet {}>'.format(self.text)
-    
