76ce2bf8a3bf727cae9fb9e831747026faa89bf0
hyamynl619 <amy.nguyenle619@gmail.com>
2020-02-26 21:29:36 -0500

    models

--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -1,25 +1,11 @@
-from flask_sqlalchemy import SQLAlchemy
-
+from flask_sqlalchemy import SQLAlchemy 
 
 DB = SQLAlchemy()
 
-
-class User (DB.Model):
-    """ Twitter users that we analyze """
+class User(DB.Model):
     id = DB.Column(DB.BigInteger, primary_key=True)
-    name = DB.Column(DB.String(15))
-    newest_tweet_id = DB.column(DB.BigInteger)
+    name = DB.Column(DB.String(15), nullable=False)
 
-    def __repr__(self):
-        return '<User {}>'.format(self.name)
-
-
-class Tweet (DB.Model):
-    """Tweets we pull"""
+class Tweet(DB.Model):
     id = DB.Column(DB.BigInteger, primary_key=True)
     text = DB.Column(DB.Unicode(300))
-    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
-    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
-
-    def __repr__(self):
-        return 'Tweet {}>'.format(self.text)
