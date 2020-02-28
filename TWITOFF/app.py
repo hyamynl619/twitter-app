efc5d34debbd72e13e2aac45bb7be51dce874c16
hyamynl619 <amy.nguyenle619@gmail.com>
2020-02-27 23:09:32 -0500

    adding buttons

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -4,6 +4,9 @@ from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
 
+from .twitter import add_or_update_user
+
+
 #make our app factory
 
 def create_app():
@@ -24,10 +27,30 @@ def create_app():
         return render_template('base.html', title = 'Home', 
         users=users)
     
+   
     @app.route('/reset')
 
+#this is to drop old DB and restart it whenever we make changes to this file
     def reset():
         DB.drop_all()
         DB.create_all()
         return render_template('base.html', title = 'Reset', users=[])
+
+#we are adding a route to input user names and a method where users that aren't available to be added
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try: 
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name,e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets, message=message)
+
+
     return app
