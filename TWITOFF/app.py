195484ee6be25fe6773d5bbdc681f2ca7b8d8b94
hyamynl619 <amy.nguyenle619@gmail.com>
2020-02-27 23:16:57 -0500

    fixing bugs

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -7,50 +7,44 @@ from .models import DB, User
 from .twitter import add_or_update_user
 
 
-#make our app factory
+# make our app factory
 
 def create_app():
     app = Flask(__name__)
 
-    #add config for database
+    # add config for database
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
     app.config['SQLAlCHEMY_TRACK_MODFICATIONS'] = False
 
-    
-    #have the database know about the app
+    # have the database know about the app
     DB.init_app(app)
 
     @app.route('/')
-    
     def root():
         users = User.query.all()
-        return render_template('base.html', title = 'Home', 
-        users=users)
-    
-   
-    @app.route('/reset')
+        return render_template('base.html', title='Home',
+                               users=users)
 
-#this is to drop old DB and restart it whenever we make changes to this file
+    @app.route('/reset')
+# this is to drop old DB and restart it whenever we make changes to this file
     def reset():
         DB.drop_all()
         DB.create_all()
-        return render_template('base.html', title = 'Reset', users=[])
+        return render_template('base.html', title='Reset', users=[])
 
-#we are adding a route to input user names and a method where users that aren't available to be added
+# we are adding a route to input user names and a method where users that aren't available to be added
     @app.route('/user', methods=['POST'])
     @app.route('/user/<name>', methods=['GET'])
-    
     def user(name=None, message=''):
         name = name or request.values['user_name']
-        try: 
+        try:
             if request.method == 'POST':
                 add_or_update_user(name)
                 message = "User {} successfully added".format(name)
             tweets = User.query.filter(User.name == name).one().tweets
         except Exception as e:
-            message = "Error adding {}: {}".format(name,e)
+            message = "Error adding {}: {}".format(name, e)
             tweets = []
         return render_template('user.html', title=name, tweets=tweets, message=message)
-
 
     return app
